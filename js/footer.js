3b917b0aa5918230f9560deaeabe86d2094b8e46
Vnkus Lande <vnkus.lande@qq.com>
2026-02-10 00:13:31 +0800

    updata footer

--- js/footer.js
+++ js/footer.js
@@ -1,9 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const currentYear = new Date().getFullYear();
-    const footerText = `© ${currentYear} Github@<a href="https://github.com/landeyucc" target="_blank" title="GitHub">landeyucc</a>. Powered by <a href="https://coldsea.vip/" target="_blank" title="Coldsea">Coldsea Team</a> `;
+    const footerText = `© ${currentYear} <a href="https://github.com/landeyucc" target="_blank">@landeyucc</a>&nbsp;All rights reserved. <span style="display: inline-block; width: 0; height: 0; overflow: hidden;">&nbsp;</span><br class="footer-br" style="display: none;" /><span style="display: none;">&nbsp;</span>Powered by <a href="https://coldsea.vip/" target="_blank" style="text-decoration: none;"><span style="font-family: 'Frizon', sans-serif;">Coldsea</span></a>&nbsp;Team.`;
     const footerElements = document.querySelectorAll('.footer p');
     
     footerElements.forEach(function(footer) {
         footer.innerHTML = footerText;
     });
+
+    function handleBrDisplay() {
+        const brElements = document.querySelectorAll('.footer-br');
+        if (window.innerWidth < 768) {
+            brElements.forEach(br => {
+                br.style.display = 'inline';
+            });
+        } else {
+            brElements.forEach(br => {
+                br.style.display = 'none';
+            });
+        }
+    }
+
+    handleBrDisplay();
+    window.addEventListener('resize', handleBrDisplay);
 });
